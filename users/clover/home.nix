b644106143638788043f5ba32f5d9a83ca781aed
Gary Glover <dev@thebigg69.co.uk>
2023-08-14 18:16:38 +0100

    Git config for ssh keys and hosts

--- users/clover/home.nix
+++ users/clover/home.nix
@@ -1,11 +1,13 @@
 {config, pkgs, pkgs-unstable, ...}:
 
-{
+let
+  homeDir = "/home/clover";
+in {
   # Home manager install and configure self
   programs.home-manager.enable = true;
 
   home.username = "clover";
-  home.homeDirectory = "/home/clover";
+  home.homeDirectory = homeDir;
   home.stateVersion = "23.05";
 
   imports = [
@@ -23,4 +25,16 @@
 
     age sops ssh-to-age # Nix-sops secrets management
   ];
+
+  programs.ssh.enable = true;
+  programs.ssh.matchBlocks = {
+    "dev.azure.com" = {
+      hostname = "dev.azure.com";
+      identityFile = "${homeDir}/.ssh/id_ed25519_dev.azure.com";
+    };
+    "github.com" = {
+      hostname = "github.com";
+      identityFile = "${homeDir}/.ssh/id_ed25519_github.com";
+    };
+  };
 }

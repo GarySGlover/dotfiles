581d0ec56d21700c509e4a931bc86ecaa3fe98a7
Gary Glover <dev@thebigg69.co.uk>
2023-11-05 11:18:38 +0000

    More emacs updates

--- modules/users/global/editors/emacs/packages/shells.nix
+++ modules/users/global/editors/emacs/packages/shells.nix
@@ -8,9 +8,7 @@ with lib; {
   config = mkIf config.wolf.editors.emacs.enable {
     programs.emacs.extraPackages = epkgs:
       with epkgs; [
-        mini-frame
-        posframe
-        transient
+        eat
       ];
   };
 }

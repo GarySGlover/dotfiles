946d3006e6b3bcecc52d8f039bcf4679aa4a1448
Gary Glover <dev@thebigg69.co.uk>
2024-01-12 17:16:37 +0000

    Devdocs

--- modules/users/global/editors/emacs/packages/help.nix
+++ modules/users/global/editors/emacs/packages/help.nix
@@ -8,6 +8,7 @@ with lib; {
   config = mkIf config.wolf.roles.editing {
     programs.emacs.extraPackages = epkgs:
       with epkgs; [
+        devdocs
         helpful
         which-key
         which-key-posframe

f0074081fbca775aca7aa005aa4bf578fc089c63
Gary Glover <dev@thebigg69.co.uk>
2024-05-09 08:17:29 +0100

    Store verify

--- scripts/nix-cleanup.sh
+++ scripts/nix-cleanup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 pushd $(dirname -- "$0") > /dev/null && pushd $(git rev-parse --show-toplevel) >> /dev/null || exit 1
 
+nix-store --verify
 ./scripts/nix-trim-generations.sh 1 0 home-manager
 nix-collect-garbage -d
 sudo nix-collect-garbage -d
--- scripts/nix-flake-rebuild.sh
+++ scripts/nix-flake-rebuild.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+nix-store --verify
 pushd $(dirname -- "$0") > /dev/null && pushd $(git rev-parse --show-toplevel) >> /dev/null || exit 1
 # sudo nixos-rebuild switch --flake .#$1
 sudo nixos-rebuild boot --flake .#$1 --show-trace

b6c3c0ecfafe2e939643e617ae1f38dedd4af26a
Gary Glover <dev@thebigg69.co.uk>
2024-05-09 08:42:33 +0100

    Org table markdown export

--- modules/users/global/editors/emacs/packages/packages.nix
+++ modules/users/global/editors/emacs/packages/packages.nix
@@ -33,6 +33,7 @@ in {
   config = mkIf config.wolf.roles.editing {
     programs.emacs.extraPackages = epkgs:
       with epkgs; [
+        ox-gfm
         ace-window
         auto-yasnippet
         avy

97506ed58bfb7b801b589bb6cebbaae68e8676b0
Gary Glover <dev@thebigg69.co.uk>
2024-06-19 20:56:56 +0100

    Update theme
    breaks indent-bars loading on daemon init

--- modules/users/global/editors/emacs/packages/manual/indent-bars.nix
+++ modules/users/global/editors/emacs/packages/manual/indent-bars.nix
@@ -8,13 +8,13 @@
 melpaBuild {
   pname = "indent-bars";
   ename = "indent-bars";
-  version = "20240228.0.2.3";
+  version = "20240601.0.6.2";
 
   src = fetchFromGitHub {
     owner = "jdtsmith";
     repo = "indent-bars";
-    rev = "fb1a0d6d1ae0131898dde9722ad44da2bbebc66f";
-    sha256 = "sha256-tkjy7uNYpnI9IZwizqbJDIS3Yf6U7KhWfoyXbBoCfnk=";
+    rev = "2216793de857cc21a1faba95361446dacdc8f3f5";
+    sha256 = "sha256-I+W3zXk7lza8XTuw/O7sXlXfn8wALtO6kMlj+nF5nfE=";
   };
 
   # Require to prevent file not found error

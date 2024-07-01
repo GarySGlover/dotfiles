2c69c8b7a72678845fb65f9cfa7bf290830ea5f2
Gary Glover <dev@thebigg69.co.uk>
2024-07-01 07:36:47 +0100

    Update compilation to check only

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,19 +1,11 @@
 ((nil
   .
-  ((eval
-    .
-    (progn
-      (setq
-       compile-command
-       (concat
-        "nix build .#homeConfigurations."
-        (user-login-name)
-        "@"
-        (system-name)
-        ".activationPackage --extra-experimental-features nix-command --extra-experimental-features flakes --show-trace && ./result/activate"))
-      (add-hook 'after-save-hook
-                (lambda ()
-                  (let ((default-directory
-                         (project-root (project-current t))))
-                    (compile compile-command)))
-                nil 'local))))))
+  ((eval .
+         (progn
+           (setq compile-command "nix flake check")
+           (add-hook 'after-save-hook
+                     (lambda ()
+                       (let ((default-directory
+                              (project-root (project-current t))))
+                         (compile compile-command)))
+                     nil 'local))))))

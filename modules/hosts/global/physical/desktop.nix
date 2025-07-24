79010b63527fb05cbe9f002bcc872bc94cbf6ee7
Gary Glover <vivid.year7953@fourleafclover.uk>
2025-07-24 19:13:38 +0100

    fix(hardware): add udev rules to set webcam power_line_frequency for improved frame rate
    - Added udev rules in desktop.nix to automatically set `power_line_frequency=1` on video devices (cameras and webcams) using v4l-utils.
    - This resolves webcam frame rate issues.
    - Also includes pkgs argument for access to v4l-utils in the configuration.

--- modules/hosts/global/physical/desktop.nix
+++ modules/hosts/global/physical/desktop.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 with lib;
@@ -26,5 +27,13 @@ with lib;
       alsa.support32Bit = true;
       pulse.enable = true;
     };
+
+    # Fix webcam frame rate
+
+    services.udev.extraRules = ''
+      SUBSYSTEM=="video4linux", KERNEL=="video[0-9]*", ATTR{name}=="*camera*", RUN+="${pkgs.v4l-utils}/bin/v4l2-ctl --device=/dev/%k --set-ctrl=power_line_frequency=1"
+      SUBSYSTEM=="video4linux", KERNEL=="video[0-9]*", ATTR{name}=="*webcam*", RUN+="${pkgs.v4l-utils}/bin/v4l2-ctl --device=/dev/%k --set-ctrl=power_line_frequency=1"
+      SUBSYSTEM=="video4linux", KERNEL=="video[0-9]*", RUN+="${pkgs.v4l-utils}/bin/v4l2-ctl --device=/dev/%k --set-ctrl=power_line_frequency=1"
+    '';
   };
 }

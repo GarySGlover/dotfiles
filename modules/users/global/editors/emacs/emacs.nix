933f8034791d051c44c10bf5144cf9f5191c4c35
Gary Glover <vivid.year7953@fourleafclover.uk>
2025-10-30 17:01:51 +0000

    feat(emacs): convert to new config

--- modules/users/global/editors/emacs/emacs.nix
+++ modules/users/global/editors/emacs/emacs.nix
@@ -17,10 +17,10 @@ in
     };
     services.emacs.enable = false;
 
-    xdg.configFile."emacs-new/early-init.el".source = ./emacs-new/early-init.el;
-    xdg.configFile."emacs-new/init.el".source = ./emacs-new/init.el;
-    xdg.configFile."emacs-new/cnit-functions.el".source = ./emacs-new/cnit-functions.el;
-    xdg.configFile."emacs-new/external-programs.el".text = ''
+    xdg.configFile."emacs/early-init.el".source = ./emacs-new/early-init.el;
+    xdg.configFile."emacs/init.el".source = ./emacs-new/init.el;
+    xdg.configFile."emacs/cnit-functions.el".source = ./emacs-new/cnit-functions.el;
+    xdg.configFile."emacs/external-programs.el".text = ''
       ;;; external-programs.el --- Paths to external programs required by my emacs configuration and custom functions.  -*- lexical-binding: t; -*-
 
       ;; Copyright (C) 2025
@@ -62,50 +62,6 @@ in
       fi
     ''}";
 
-    xdg.configFile."emacs/emacs-config.el".source = ./emacs-config.el;
-    xdg.configFile."emacs/early-init.el".text = ''
-      ;; -*- lexical-binding: t -*-
-
-      (setq use-package-compute-statistics t)
-
-      (setopt gc-cons-threshold (* 50 1000 1000))
-    '';
-    xdg.configFile."emacs/init.el".text = ''
-      ;; -*- lexical-binding: t -*-
-
-      (let ((emacs-init-file (expand-file-name "emacs-config.el" "~/.config/emacs")))
-        (load-file emacs-init-file))
-
-      (use-package ef-themes
-        :init
-        (mapc #'disable-theme custom-enabled-themes)
-        (ef-themes-select '${theme.name}))
-
-      ;; Set font and font size
-      (defun cloveynit-after-frame ()
-        (set-face-attribute 'default nil :family "${theme.font.name}" :height ${toString theme.font.size}0)
-        (set-face-attribute 'fixed-pitch nil :family "${theme.font.name}" :height ${toString theme.font.size}0)
-        (set-face-attribute 'fixed-pitch-serif nil :family "${theme.font.name}" :height ${toString theme.font.size}0)
-        (set-face-attribute 'variable-pitch nil :family "${theme.font.name}" :height ${toString theme.font.size}0)
-        (let ((error-foreground (face-foreground 'error))
-              (error-background (face-background 'error)))
-          (set-face-foreground 'font-lock-comment-face error-foreground)
-          (set-face-background 'font-lock-comment-face error-background)))
-
-      (if (daemonp)
-          (add-hook 'server-after-make-frame-hook #'cloveynit-after-frame)
-        (cloveynit-after-frame))
-
-      (setq ispell-alternate-dictionary (expand-file-name "emacs/dict.txt" (xdg-config-home)))
-    ''
-    + (
-      if (hasAttr "copilot_enabled_organisations" secrets) then
-        ''
-          (defvar cnit/copilot-enabled-organisations '(${lib.strings.concatStringsSep " " secrets.copilot_enabled_organisations}))
-        ''
-      else
-        ""
-    );
     programs.git.ignores = [
       "*~"
       ".#*"

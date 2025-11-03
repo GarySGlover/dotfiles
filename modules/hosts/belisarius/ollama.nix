5b21862f334db7ad36cd88399696379deeb68036
Gary Glover <vivid.year7953@fourleafclover.uk>
2025-11-03 19:43:03 +0000

    chore(nix): remove ollama

--- modules/hosts/belisarius/ollama.nix
+++
@@ -1,8 +0,0 @@
-{
-  services.ollama = {
-    enable = true;
-    # Optional: preload models, see https://ollama.com/library
-    loadModels = [ "deepseek-r1:7b" ];
-    acceleration = "rocm";
-  };
-}

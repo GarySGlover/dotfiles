b326efe9a49230041f61d728b6da203a23b525e7
Gary Glover <vivid.year7953@fourleafclover.uk>
2026-01-13 15:46:48 +0000

    feat: remove unused treesit grammer overrides

--- modules/overlays/tree-sitter-grammars.nix
+++
@@ -1,9 +0,0 @@
-inputs: final: prev: {
-  # Ref:
-  # https://github.com/NixOS/nixpkgs/tree/master/pkgs/development/tools/parsing/tree-sitter/grammars
-  tree-sitter = prev.tree-sitter.override {
-    extraGrammars = {
-      tree-sitter-nim = final.lib.importJSON ./tree-sitter-grammars/nim.json;
-    };
-  };
-}
